6d88d10b0b6b2eca55abb27956f8819773296b00
jaicabher1 <jaicabher1@alum.us.es>
2024-07-07 11:19:09 +0200

    aux function

---
+++ IntroduccionJS/IntroduccionJS/js/funciones.js
@@ -0,0 +1,18 @@
+
+
+export function sumar(num1,num2) {
+    return num1 + num2
+}
+
+export function restar(num1,num2) {
+    return num1 - num2
+}
+
+const hola = "Hola Mundo"
+
+export default hola
+//Otra opcion cuando tienes muchas funciones
+/* export {
+    sumar,
+    restar
+} */
