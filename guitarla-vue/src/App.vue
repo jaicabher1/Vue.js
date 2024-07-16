b0bade04d661a1d9bfa30735efc141cb7005933a
jaicabher1 <jaicabher1@alum.us.es>
2024-07-16 10:52:10 +0200

    GLA: carrito persistente

--- guitarla-vue/src/App.vue
+++ guitarla-vue/src/App.vue
@@ -22,7 +22,15 @@ onMounted(() => {
     guitarraRef.value = db
     guitarraReactive.guitarras = db
     guitarra.value = db[5]
+    const carritoStorage = localStorage.getItem('carrito')
+    if(carritoStorage) {
+        carrito.value = JSON.parse(carritoStorage)
+    }
 })
+
+const guardarLocalStorage = () => {
+    localStorage.setItem('carrito', JSON.stringify(carrito.value))
+}
 
 const agregarCarrito = (guitarra) => {
     const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
@@ -33,6 +41,7 @@ const agregarCarrito = (guitarra) => {
         guitarra.cantidad = 1
         carrito.value.push(guitarra)
     }
+    guardarLocalStorage()
 
 
 }
@@ -41,6 +50,7 @@ const decrementarCantidad = (id) => {
     const index = carrito.value.findIndex(producto => producto.id === id)
     if(carrito.value[index].cantidad <= 1) return
     carrito.value[index].cantidad--
+    guardarLocalStorage()
     
 }
 
@@ -48,14 +58,17 @@ const decrementarCantidad = (id) => {
 const incrementarCantidad = (id) => {
     const index = carrito.value.findIndex(producto => producto.id === id)
     carrito.value[index].cantidad++
+    guardarLocalStorage()
 }
 
 const eliminarProducto = (id) => {
     carrito.value = carrito.value.filter(producto => producto.id !== id)
+    guardarLocalStorage()
 }
 
 const vaciarCarrito = () => {
     carrito.value = []
+    guardarLocalStorage()
 }
 
 
