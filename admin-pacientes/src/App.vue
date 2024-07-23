600dd92eed6c06fcb4d0349786d8cd49de1de666
jaicabher1 <jaicabher1@alum.us.es>
2024-07-23 21:39:39 +0200

    formulario component

--- admin-pacientes/src/App.vue
+++ admin-pacientes/src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
+import Header from './components/Header.vue'
+import Formulario from './components/Formulario.vue'
 
 </script>
 
 <template>
-  <h1 class="text-6xl from-teal-400">Hola Mundo Tailwind</h1>
+  <div class="container mx-auto mt-20">
+    <Header />
+    <br>
+    <div class="mt:12 md:flex">
+      <Formulario />
+    </div>
+
+  </div>
 </template>
