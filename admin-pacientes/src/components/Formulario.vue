0530d19c92740c1b8000c9c37a923b7c452d1060
jaicabher1 <jaicabher1@alum.us.es>
2024-08-01 09:56:27 +0200

    Register alert

--- admin-pacientes/src/components/Formulario.vue
+++ admin-pacientes/src/components/Formulario.vue
@@ -1,4 +1,31 @@
-<script>
+<script setup>
+import { reactive } from "vue";
+import Alerta from "./Alerta.vue";
+
+const alerta = reactive({
+  tipo: "",
+  mensaje: "",
+});
+
+const paciente = reactive({
+  nombre: "",
+  propietario: "",
+  email: "",
+  alta: "",
+  sintomas: "",
+});
+
+const validar = () => {
+  if (Object.values(paciente).includes("")) {
+    alerta.mensaje = "Todos los campos son obligatorios";
+    alerta.tipo = "error";
+    return;
+  } else {
+    alerta.mensaje = "Paciente registrado correctamente";
+    alerta.tipo = "exito";
+  }
+  
+};
 </script>
 
 
@@ -10,7 +37,12 @@
       Añade Pacientes y
       <span class="text-indigo-600 font-bold">Adminístralos</span>
     </p>
-    <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+    <Alerta v-if="alerta.mensaje" :alerta="alerta" />
+
+    <form
+      class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      @submit.prevent="validar"
+    >
       <div class="mb-5">
         <label for="mascota" class="block text-gray-700 uppercase font-bold">
           Nombre Mascota
@@ -20,6 +52,7 @@
           type="text"
           placeholder="Nombre de la mascota"
           class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          v-model="paciente.nombre"
         />
       </div>
       <div class="mb-5">
@@ -34,6 +67,7 @@
           type="text"
           placeholder="Nombre del propietario"
           class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          v-model="paciente.propietario"
         />
       </div>
       <div class="mb-5">
@@ -45,6 +79,7 @@
           type="email"
           placeholder="Email del propietario"
           class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          v-model="paciente.email"
         />
       </div>
       <div class="mb-5">
@@ -55,6 +90,7 @@
           id="alta"
           type="date"
           class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          v-model="paciente.alta"
         />
       </div>
       <div class="mb-5">
@@ -65,6 +101,7 @@
           id="sintomas"
           placeholder="Síntomas que presenta"
           class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+          v-model="paciente.sintomas"
         />
       </div>
       <input
